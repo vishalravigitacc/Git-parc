45a45067afb32e4bdad768aeea4399df0d86e7a6
vishalravi <vishalravi08@gmail.com>
2022-12-21 12:00:00 +0530

    my task

---
+++ src/org/tcs/Sample.java
@@ -0,0 +1,26 @@
+package org.tcs;
+
+public class Sample {
+
+	
+	
+	private void empId() {
+System.out.println("my emp id is : 453");
+	}
+	
+	private void empName() {
+System.out.println("my emp name is : sri");
+	}
+	public static void main(String[] args) {
+	
+		
+
+		Sample S = new Sample();
+		S.empId();
+		S.empName();
+	}
+	
+	
+	
+	
+}
